c006dafe3c24b23c3a8ed10e72b5ff5f59696b28
Wilson Ching <Wilson Ching>
2017-05-30 20:46:05 +0100

    test class and eslint fix

---
+++ test.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const ava = require('ava')
+const test = ava.test
+const GithubGraphQLApi = require('.')
+const nock = require('nock')
+
+const apiQuery = `
+  viewer {
+    login
+  }`
+
+const apiResponse = {
+  'data': {
+    'viewer': {
+      'login': 'wilsonchingg'
+    }
+  }
+}
+
+test.before(() => {
+  nock.disableNetConnect()
+})
+
+test('post /apps without params', t => {
+  let github = new GithubGraphQLApi()
+  let api = nock('https://api.github.com')
+  .post('/graphql')
+  .reply(201, apiResponse)
+
+  return github.query(apiQuery).then(() => {
+    t.pass()
+    api.done()
+  })
+})
+
+test('post /apps with params', t => {
+  let github = new GithubGraphQLApi({
+    userAgent: 'Test',
+    proxy: 'http://proxy.test.com'
+  })
+  let api = nock('https://api.github.com')
+  .post('/graphql')
+  .reply(201, apiResponse)
+  t.pass()
+  github.query(apiQuery, () => {
+    t.pass()
+    api.done()
+  })
+})
